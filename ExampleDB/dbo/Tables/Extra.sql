69e4a32348783bcb19aa25bc714dde069b4dbbb6
Cavan Daniel Lamprecht <cavan@savilleconsulting.co.za>
2018-02-08 13:04:52 +0200

    Added the database

---
+++ ExampleDB/dbo/Tables/Extra.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Extra] (
+    [Recordid]  INT IDENTITY (1, 1) NOT NULL,
+    [Usersid]   INT NOT NULL,
+    [Age]       INT NOT NULL,
+    [isDeleted] INT CONSTRAINT [DF_Extra_isDeleted] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_Extra] PRIMARY KEY CLUSTERED ([Recordid] ASC),
+    CONSTRAINT [FK_Extra_Users] FOREIGN KEY ([Usersid]) REFERENCES [dbo].[Users] ([Recordid])
+);
+
---
+++ ExampleDB/dbo/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Recordid]  INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      VARCHAR (255) NOT NULL,
+    [Surname]   VARCHAR (255) NULL,
+    [isDeleted] INT           CONSTRAINT [DF_Users_isDeleted] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Recordid] ASC)
+);
+
